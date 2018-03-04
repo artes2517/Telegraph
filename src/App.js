5bc1bb8da0301aedf86e93b2f7dc4836554ff7f1
artes2517 <bro2517@yandex.ru>
2018-03-04 10:45:43 +0300

    Created Main page app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      author: '',
+      canEdit: true,
+    }
+    this.onTitleChange = (e) => this.setState({ title: e.target.value })
+    this.onAuthorChange = (e) => this.setState({ author: e.target.value })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="App-main">
+          <header className="App-header">
+            <h1>
+              {this.state.title && <label htmlFor="title">Title</label>}
+              <input
+                id="title"
+                type="text"
+                className="input__lg"
+                value={this.state.title}
+                onChange={this.onTitleChange}
+                placeholder="Title"
+                autoComplete="off"
+                readOnly={!this.state.canEdit}
+              />
+            </h1>
+            <address>
+              {this.state.author && <label htmlFor="author">Author</label>}
+              <input
+                id="author"
+                type="text"
+                className="input__sm"
+                value={this.state.author}
+                onChange={this.onAuthorChange}
+                placeholder="Your name"
+                autoComplete="off"
+                readOnly={!this.state.canEdit}
+              />
+            </address>
+          </header>
+          <article className="App-article">
+          </article>
+          <aside className="App-aside">
+            <button className="button">PUBLISH</button>
+          </aside>
+        </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
