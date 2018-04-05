1ca5adb6d46bb59c9ff85b3d5985b3a34563abe9
artes2517 <bro2517@yandex.ru>
2018-04-05 21:24:54 +0300

    Create mongoose model story

---
+++ server/models/story.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const idvalidator = require('mongoose-id-validator');
+
+const StorySchema = new Schema({
+  title: {
+    type: String,
+    required: [true]
+  },
+  author: {
+    type: String,
+    required: [true]
+  },
+  discription: {
+    type: String,
+    required: [true]
+  },
+  dataTime: {
+    type: Date,
+    default: Date.now(),
+    required: [true]
+  }
+});
+
+StorySchema.plugin(idvalidator);
+
+const Story = mongoose.model('story', StorySchema);
+
+module.exports = Story;
